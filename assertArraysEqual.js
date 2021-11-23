81a460e6840d0f7b67a4317575e6fab7705e08b6
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-23 17:35:13 -0500

    Almost done

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,8 @@
+const assertArraysEqual = function (arr1, arr2) {
+  return eqArrays(arr1, arr2) === true ? console.log(`👍 👍 👍 Arrays are equal`) : console.log(`👎👎👎Arrays are not equal`);
+};
+
+
 const eqArrays = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -9,3 +14,9 @@ const eqArrays = (arr1, arr2) => {
   }
   return true;
 };
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]);// => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
