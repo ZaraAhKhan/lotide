06c4479b553d8a9d28d186250ebbe3886523b91d
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-24 16:39:00 -0500

    create a function countLetters

---
+++ countLetters.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual,expected) {
+  (actual === expected ? console.log(`👍 👍 👍 Assertion Passed:${actual} === ${expected}`) : console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`));
+};
+
+const countLetters = function(string) {
+
+};
