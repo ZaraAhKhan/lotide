72fd60e09907878a34c89ec3f3fa644d9a3604f9
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-25 18:45:59 -0500

    function to learn callback function

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function(arr1, arr2) {
+  return eqArrays(arr1, arr2) === true ? console.log(`👍 👍 👍 Arrays are equal`) : console.log(`👎👎👎Arrays are not equal`);
+};
+
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+const takeUntil = function(array, callBack) {
+  let resultArr = [];
+  for (let element of array) {
+    if (callBack(element)) {
+      return resultArr;
+    } else {
+      resultArr.push(element);
+    }
+  }
+};
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2,[ "I've", 'been', 'to', 'Hollywood' ]);
