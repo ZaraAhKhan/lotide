31ef15538c9aacfd7744e3366d75b3e76bb0c6df
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-25 20:30:44 -0500

    Lotide proj

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual,expected) {
+  (actual === expected ? console.log(`👍 👍 👍 Assertion Passed:${actual} === ${expected}`) : console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`));
+};
+const findKey = function(object, callBack) {
+  for (let key in object) {
+    if (callBack(object[key])) {
+      return key;
+    }
+
+  }
+  return undefined;
+};
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
