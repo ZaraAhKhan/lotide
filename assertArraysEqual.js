39dee3c2469d82be7cbd28a709b43f8e357ae559
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-29 09:32:17 -0500

    refactor to add module and create test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,8 @@
+const eqArrays = require('./eqArray');
+
 const assertArraysEqual = function(arr1, arr2) {
   return eqArrays(arr1, arr2) === true ? console.log(`👍 👍 👍 Arrays are equal`) : console.log(`👎👎👎Arrays are not equal`);
 };
 
+module.exports = assertArraysEqual;
 
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]);// => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
