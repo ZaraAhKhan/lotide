2b6c47703a4cde9eda62a6eb9686d7c17590eb84
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-29 08:32:58 -0500

    refactor to add module,removes tests and add test file

--- tail.js
+++ tail.js
@@ -10,16 +10,6 @@ const tail = (array) => {
   }
   return answer;
 };
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
 
-const solution = tail(["Hello"]);
-assertEqual(solution.length , 0);
-const solution2 = (tail([]));
-assertEqual(solution2.length , 0);
