8ac3d0de072f172249e8699eb9e9dc76bb61865a
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-11-29 10:15:43 -0500

    refactor to use mocha testing framework  and chai assert library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const solution = tail(["Hello"]);
-assertEqual(solution.length , 0);
-const solution2 = (tail([]));
-assertEqual(solution2.length , 0);
+describe("#tail", () => {
+  it('returns ["Lighthouse","Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]),["Lighthouse","Labs"]);
+  });
+  it('returns ["Lighthouse","Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse","Labs"]);
+  });
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
+});
